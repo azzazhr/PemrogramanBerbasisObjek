51c14e08fa54d04e133c6125d53e458b27626b0e
azzazhr <azzahraqina70@gmail.com>
2024-09-13 10:39:39 +0700

    add percobaan 2 jobsheet 3

--- minggu03/MotorEncapsulation/Motor.java
+++ minggu03/MotorEncapsulation/Motor.java
@@ -1,8 +1,33 @@
 package minggu03.MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kotakOn = false;
+    private int kecepatan = 0;
+    private boolean kotakOn = false;
+
+    public void nyalakanMesin() {
+        kotakOn = true;
+    }
+
+    public void matikanMesin() {
+        kotakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kotakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kotakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
+    }
 
     public void printStatus() {
         if (kotakOn == true) {
