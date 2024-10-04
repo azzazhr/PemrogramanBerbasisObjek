6685e018f4acfd84bd01bfbef5a5793185be8acd
azzazhr <azzahraqina70@gmail.com>
2024-10-04 14:07:44 +0700

    add percobaan 1 jobsheet 6

---
+++ minggu06/ClassA.java
@@ -0,0 +1,11 @@
+package minggu06;
+
+public class ClassA {
+    public int x;
+    public int y;
+
+    public void getNilai() {
+        System.out.println("nilai x:" + x);
+        System.out.println("nilai y:" + y);
+    }
+}
